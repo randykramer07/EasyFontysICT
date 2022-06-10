ddcb56bd48eef27a0255775e5beda237602272fe
randykramer07 <100212457+randykramer07@users.noreply.github.com>
2022-06-10 10:49:00 +0200

    Update randykramer-apache2.sh

--- Apache/randykramer-apache2.sh
+++ Apache/randykramer-apache2.sh
@@ -7,7 +7,7 @@ echo
 echo Aan de hand van dit script kun jij makkelijk in 1x een Apache website installeren
 echo =================================================================================
 echo Wil jij een eigen URL gebruiken?
-read jn
+read -p "(J)a of (N)ee"jn
 	case $jn in
         	[Jj]* ) eigenURL;;
         	[Nn]* ) geenURL;;
